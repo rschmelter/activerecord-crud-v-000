0501992e5832f0105cba2861d1a9d4a6f63bee81
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-13 18:43:02 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20180213180924) do
 
+  create_table "movies", force: :cascade do |t|
+    t.string  "title"
+    t.integer "release_date"
+    t.string  "director"
+    t.string  "lead"
+    t.boolean "in_theaters"
+  end
+
 end
