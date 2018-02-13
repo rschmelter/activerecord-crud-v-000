eb88f81f20b0c27192641142646ce864a9691a63
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-13 20:18:38 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -80,7 +80,9 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movie.each do |t|
+    t.title = "A Movie"
+  end
 end
 
 def can_destroy_a_single_item
